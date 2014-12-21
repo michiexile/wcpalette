dafbf5aa13c41d8be90754bce52be05e383b12d5
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2014-12-21 13:15:30 +0100

    wip

--- colorwheel.js
+++ colorwheel.js
@@ -25,12 +25,11 @@ function setupSelector(err, d) {
     drawSelected();
 }
 function resort(condition) {
+    d3.select('#colorselect').selectAll('option').remove();
     opts = d3.select('#colorselect')
 	.selectAll('option')
-        .remove()
 	.data(hpdata)
 	.sort(function(a,b){return d3.ascending(a[condition], b[condition]);})
-        .enter()
 	.append('option')
 	.attr('label', function(r) {return r['IDX'];})
         .text(function(r) {return r['PAINT - MARKETING NAME'] +": "+ r['MANUFACTURER'] +" "+ r['CODE']; });
